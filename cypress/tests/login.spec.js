af43718c4290d4bf103a17926b950a9a9c273de4
Daniel Moreno <morennodanniel@gmail.com>
2026-03-24 19:58:51 -0300

    docs: adiciona evidências e matriz de testes do projeto RWA

--- cypress/tests/login.spec.js
+++ cypress/tests/login.spec.js
@@ -1,7 +1,6 @@
 describe('Login Success', () => {
   it('Deve fazer login com um usuário válido', () => {
-    cy.visit('/signin')
-
+    cy.visit('/signin') 
     cy.get('[name="username"]').type('Heath93')
     cy.get('[name="password"]').type('s3cret')
     cy.get('[type="submit"]').click()
@@ -12,7 +11,6 @@ describe('Login Success', () => {
 describe('Login failed', () => {
   it('Deve exibir mensagem de erro ao usar credenciais inválidas', () => {
     cy.visit('/signin')
-
     cy.get('[name="username"]').type('daniel')
     cy.get('[name="password"]').type('123456')
     cy.get('[type="submit"]').click()
